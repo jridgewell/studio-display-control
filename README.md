421a2534df10aae118a4e48eedada3e54290f9d5
Justin Ridgewell <justin@ridgewell.name>
2024-04-19 01:22:11 -0400

    Handle multiple displays

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 Set the brightness of an Apple Studio Display in Node.js.
 
-
 ## Why?
 
 So that I can control the brightness in Windows.
-
 
 ## Installation
 
@@ -14,22 +12,23 @@ So that I can control the brightness in Windows.
 npm install studio-display-control
 ```
 
-
 ## Usage
 
 ```typescript
-import { hasDisplay, getBrightness, setBrightness } from 'studio-display-control';
+import { getDisplays } from 'studio-display-control';
 
-// Check if a Studio Display is connected
-hasDisplay(); // true
+for (const display of getDisplays()) {
+  // Get the current brightness as a percent.
+  console.log(await display.getBrightness()); // 100
 
-// Get the current brightness as a percent.
-console.log(getBrightness()); // 100
+  // Set the brightness as a percent.
+  console.log(await display.setBrightness(50));
+  // And it's reflected on the next get.
+  console.log(await display.getBrightness()); // 50
 
-//Set the brightness as a percent.
-console.log(setBrightness(50));
-
-console.log(getBrightness()); // 50
+  // Access the serial number of the display.
+  console.log(await display.getSerialNumber());
+}
 ```
 
 # License
--- src/studio-display-control.ts
+++ src/studio-display-control.ts
@@ -1,4 +1,4 @@
-import { Device, findByIds, Interface } from 'usb';
+import { Device, Interface, getDeviceList } from 'usb';
 import { promisify } from 'util';
 
 const BRIGHTNESS_MIN = 400.0;
@@ -26,67 +26,114 @@ const SD_REPORT_ID = 0x01;
 const SD_PRODUCT_ID = 0x1114;
 const SD_VENDOR_ID = 0x05ac;
 
-function getDisplay() {
-  return findByIds(SD_VENDOR_ID, SD_PRODUCT_ID);
-}
-
-export function hasDisplay() {
-  return !!getDisplay();
-}
-
-export async function getBrightness() {
-  const display = getDisplay();
-  if (!display) throw new Error(`can't connect to studio display`);
-
-  const data = await controlTransfer(display, Direction.In, Report.Get, 7);
-  return nitsToPercent(data.readUInt16LE(1));
-}
-
-export async function setBrightness(percent: number) {
-  if (percent < 0 || percent > 100) throw new Error('expected percent within range [0, 100]');
-
-  const display = getDisplay();
-  if (!display) throw new Error(`can't connect to studio display`);
-
-  const data = makeRequestData(percentToNits(percent));
-  await controlTransfer(display, Direction.Out, Report.Set, data);
-}
-
 const ifaceRelease = promisify(Interface.prototype.release);
 const devControlTransfer = promisify(Device.prototype.controlTransfer);
-async function controlTransfer<D extends number | Buffer>(
-  display: Device,
-  dir: Direction,
-  report: Report,
-  data: D,
-): Promise<D extends number ? Buffer : number> {
-  const dispose: Array<() => void | Promise<void>> = [];
-  try {
-    display.open();
-    dispose.unshift(() => display.close());
+const devGetStringDesc = promisify(Device.prototype.getStringDescriptor);
 
-    const iface = display.interface(SD_BRIGHTNESS_INTERFACE);
-    iface.claim();
-    dispose.unshift(() => ifaceRelease.call(iface));
+export function getDisplays() {
+  const devices = getDeviceList();
+  const displays: Display[] = [];
+  for (const dev of devices) {
+    const desc = dev.deviceDescriptor;
+    if (desc.idVendor === SD_VENDOR_ID && desc.idProduct === SD_PRODUCT_ID) {
+      displays.push(new Display(dev));
+    }
+  }
 
-    const out = await devControlTransfer.call(
-      display,
-      makeRequestType(dir),
-      report,
-      HID_REPORT_TYPE_FEATURE | SD_REPORT_ID,
-      SD_BRIGHTNESS_INTERFACE,
-      data,
-    );
-    return out as any;
-  } finally {
-    for (const d of dispose) {
+  return displays;
+}
+
+class Display {
+  private _device: Device;
+  private _queue = 0;
+  private _open: null | Device = null;
+  private _claimed: null | Interface = null;
+
+  constructor(dev: Device) {
+    this._device = dev;
+  }
+
+  async getBrightness() {
+    const response = await this._guard(() => controlTransfer(this._device, Direction.In, Report.Get, 7));
+    return nitsToPercent(response.readUInt16LE(1));
+  }
+
+  async setBrightness(percent: number) {
+    if (percent < 0 || percent > 100) throw new Error('expected percent within range [0, 100]');
+
+    const data = makeRequestData(percentToNits(percent));
+    await this._guard(() => controlTransfer(this._device, Direction.Out, Report.Set, data));
+  }
+
+  async getSerialNumber() {
+    return this._guard(() => {
+      const display = this._device;
+      const desc = display.deviceDescriptor;
+      return devGetStringDesc.call(display, desc.iSerialNumber);
+    });
+  }
+
+  private async _guard<R>(
+    fn: () => R,
+  ): Promise<R> {
+    try {
+      this._enter();
+      return await fn();
+    } finally {
+      await this._exit();
+    }
+  }
+
+  private _enter() {
+    const display = this._device;
+    this._queue++;
+    if (!this._open) {
+      display.open();
+      this._open = display;
+    }
+    if (!this._claimed) {
+      const iface = display.interface(SD_BRIGHTNESS_INTERFACE);
+      iface.claim();
+      this._claimed = iface;
+    }
+    return display;
+  }
+
+  private async _exit() {
+    if (--this._queue === 0) {
       try {
-        await d();
+        if (this._claimed) {
+          await ifaceRelease.call(this._claimed);
+          if (this._queue > 0) return;
+          this._claimed = null;
+        }
       } catch (e) {
-        console.error('additional error', e);
+        console.error('failed to release interface', e);
+      }
+      try {
+        this._open?.close();
+        this._open = null;
+      } catch (e) {
+        console.error('failed to close device', e);
       }
     }
   }
+}
+
+function controlTransfer<D extends number | Buffer>(
+  display: Device,
+  direction: Direction,
+  report: Report,
+  data: D,
+): Promise<D extends number ? Buffer : number> {
+  return devControlTransfer.call(
+    display,
+    makeRequestType(direction),
+    report,
+    HID_REPORT_TYPE_FEATURE | SD_REPORT_ID,
+    SD_BRIGHTNESS_INTERFACE,
+    data,
+  ) as any;
 }
 
 function makeRequestType(dir: Direction) {
